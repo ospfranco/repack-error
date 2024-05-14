0242195ed6854c04deda5f9630d7b88c2735c083
ospfranco <ospfranco@gmail.com>
2024-05-14 14:25:56 +0200

    INITIAL COMMIT

---
+++ ios/blahblah/AppDelegate.mm
@@ -0,0 +1,48 @@
+#import "AppDelegate.h"
+
+#import <NativeSampleModule.h>
+#import <React/CoreModulesPlugins.h>
+#import <React/RCTBundleURLProvider.h>
+#import <ReactCommon/RCTTurboModuleManager.h>
+
+@interface AppDelegate () <RCTTurboModuleManagerDelegate> {
+}
+@end
+
+@implementation AppDelegate
+
+- (BOOL)application:(UIApplication *)application
+    didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+  self.moduleName = @"blahblah";
+  // You can add your custom initial props in the dictionary below.
+  // They will be passed down to the ViewController used by React Native.
+  self.initialProps = @{};
+
+  return [super application:application
+      didFinishLaunchingWithOptions:launchOptions];
+}
+
+- (NSURL *)sourceURLForBridge:(RCTBridge *)bridge {
+  return [self bundleURL];
+}
+
+- (NSURL *)bundleURL {
+#if DEBUG
+  return
+      [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index"];
+#else
+  return [[NSBundle mainBundle] URLForResource:@"main"
+                                 withExtension:@"jsbundle"];
+#endif
+}
+
+- (std::shared_ptr<facebook::react::TurboModule>)
+    getTurboModule:(const std::string &)name
+         jsInvoker:(std::shared_ptr<facebook::react::CallInvoker>)jsInvoker {
+  // if (name == "NativeSampleModule") {
+  //   return std::make_shared<facebook::react::NativeSampleModule>(jsInvoker);
+  // }
+  return nullptr;
+}
+
+@end
